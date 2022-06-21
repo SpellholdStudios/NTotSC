1d119392a63c14822a87bb75ed6c1d7418dd8401
Gitjas <Gitjas@users.noreply.github.com>
2022-06-21 08:24:54 +0200

    Update ntpandri.d
    Area Wood of the Dead will be revealed in dialogue with Pandris

--- ntotsc/base/d/ntpandri.d
+++ ntotsc/base/d/ntpandri.d
@@ -21,5 +21,6 @@ IF ~~ THEN BEGIN 3
   SAY @5
   IF ~~ THEN DO ~GiveItemCreate("NTPBlet3",LastTalkedToBy(Myself),0,0,0)
 AddJournalEntry(@10098,QUEST)
+RevealAreaOnMap("AR01PB")
 EscapeArea()~ UNSOLVED_JOURNAL @10053 EXIT
 END
